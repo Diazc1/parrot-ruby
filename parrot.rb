e8da3d90927338f9c0caa1bb1ed8e391f4d717f5
Cathy Diaz <Cathydiaz14@gmail.com>
2021-02-03 08:07:35 -0800

    commit tree 31f76886689137a1ad8e4c1d051038e5d75fa9c1

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(phrase = "Squawk!") #should output "Squawk!" to the terminal when called without any arguments
+    puts phrase          
+    return phrase           #should return the default phrase, "Squawk!" when called without any arguments  #should output the given phrase when called with an argument
+  end 
+
